4ff2c86afde1ced3328893b8bca825fa57798e07
Sarra Koussaier <95942823+sarrakou@users.noreply.github.com>
2023-09-29 17:41:25 +0200

    Update PlayerMovementScript.cs

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -38,16 +38,6 @@ public class PlayerMovementScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (m_Side == SIDE.Mid)
-    [SerializeField]
-    private float speedAuto;
-
-
-    void Update()
-    {
-        transform.position += transform.forward * Time.deltaTime * speedAuto;
-        if(Input.GetKeyDown(KeyCode.A)) 
-        { 
-            if(m_Side == SIDE.Mid)
             {
                 NewXPos = -xValue;
                 m_Side = SIDE.Left;
@@ -66,6 +56,7 @@ public class PlayerMovementScript : MonoBehaviour
             {
                 NewXPos = xValue;
                 m_Side = SIDE.Right;
+
             }
             else if (m_Side == SIDE.Left)
             {
